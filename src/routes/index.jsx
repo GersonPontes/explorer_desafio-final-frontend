4d15c6ae07ea966dde1d2e76312684323dabdb1a
gerson <gersonpdeoliveira@gmail.com>
2024-03-04 09:46:12 -0300

    update

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-//import { useEffect } from 'react';
 import { useAuth } from "../hooks/auth";
-//import { api } from '../services/api';
 
 import { AuthRoutes } from "./auth.routes";
 import { AdminRoutes } from "./admin.routes";
@@ -11,7 +9,6 @@ import { USER_ROLE } from "../utils/roles";
 
 export function Routes() {
   const { user } = useAuth();
-  //const { user, signOut } = useAuth();
 
   function AccessRoute() {
     switch(user.role){
@@ -23,28 +20,6 @@ export function Routes() {
         return <CustomerRoutes />
     }
   };
-
-  /*
-  useEffect(() => {
-    function validateUser(){
-      let data = localStorage.getItem("@foodexplorer:user");
-
-      if(data){
-        data = { user: JSON.parse(data)};
-        
-        api
-        .get(`/users/validated/${data.user.id}`)
-        .catch((error) => {
-          if(error.response?.status === 401) {
-            signOut()
-          }      
-        })  
-      }
-    }
-
-    validateUser();
-  }, []);
-  */
 
   return (
     <BrowserRouter>
